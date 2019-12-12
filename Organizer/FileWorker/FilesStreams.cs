a655fc4eeb43370c1590bb630b024225627fc3bc
nastya.starostenko <st.nastya96@gmail.com>
2019-12-12 21:26:51 +0200

    First commit with all files

---
+++ Organizer/FileWorker/FilesStreams.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections;
+using Organizer;
+
+namespace Organizer.FileWorker
+
+{
+  
+        internal class FilesStreams
+        {
+
+            public string writePath;
+            ReadFile notices;
+
+
+            public FilesStreams(string filePath)
+            {
+                ChangePathToFile(filePath);
+                ReadFile notices = new ReadFile();
+
+            }
+
+            public void ChangePathToFile(string filePath)
+            {
+                writePath = filePath; // @"C:\SomeDir\hta2.txt";
+                int position = filePath.LastIndexOf(@"\");
+
+
+                string directoryPath = filePath.Substring(position + 1); //@"C:\SomeDir";
+
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+            }
+
+            public void AddTextToFile()
+            {
+               
+                         try
+                         {
+                             bool replace = true;
+                                AddFilesFromFileTolist();
+                             if (notices.getCountOfList() != 0)
+                             {
+                                 Console.WriteLine("You have data in a file. Are you want replace or only add string to file? 1 - replace, 2 - only add");
+                                 replace = Helper.ReadCommandsInt() == 1 ? false : true;
+                             }
+
+
+                             Console.WriteLine("When you finish, please type key END");
+                             using (StreamWriter sw = new StreamWriter(writePath, replace, System.Text.Encoding.Default))
+                             {
+                                 ConsoleKeyInfo keyInfo;
+                                 do
+                                 {
+                                     sw.WriteLine(Console.ReadLine());
+                                     keyInfo = Console.ReadKey(true);
+                                 } while (keyInfo.Key != ConsoleKey.End);
+
+                                Console.WriteLine("You data was added to the file");
+                        //add wait
+                             }
+                         }
+                         catch (Exception e)
+                         {
+                             Console.WriteLine(e.Message);
+                         }
+                    
+            }
+
+            public void AddFilesFromFileTolist()
+            {
+                try
+                {
+                    string fileLine;
+                    notices = new ReadFile();
+                    using (StreamReader sr = new StreamReader(writePath, System.Text.Encoding.UTF8))
+                    {
+                        {
+                            notices = new ReadFile();
+                            while ((fileLine = sr.ReadLine()) != null)
+                            {
+                                notices.addLineToList(fileLine);
+                            };
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+            public void GetTextFromFileAsync()
+            {
+                try
+                {
+                    AddFilesFromFileTolist();
+                    notices.getListOfLinesFromFile(); 
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+
+    
+    }
+
---
+++ Organizer/Helper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Organizer.FileWorker;
+
+namespace Organizer
+{
+    internal class Helper
+    {
+
+        public static int ReadCommandsInt() => Convert.ToInt32(Console.ReadLine());
+    }
+    class ReadFile
+    {
+        internal List<String> filesLines;
+        internal ReadFile()
+        {
+            filesLines = new List<String>();
+        }
+        internal void addLineToList(String str)
+        {
+            filesLines.Add(str);
+        }
+
+        internal void getListOfLinesFromFile()
+        {
+            foreach (string i in filesLines)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        internal int getCountOfList() => filesLines.Count;
+
+
+    }
+
+
+    internal class OrganizerMenu
+    {
+        int programKey;
+        public OrganizerMenu()
+        {
+            Console.WriteLine("Hello, you open Orginezer!\n" + "If you want to start, please enter 1. If you want to exit please enter 0");
+
+            StartMenu(Helper.ReadCommandsInt());
+        }
+        public void StartMenu(int programKey)
+        {
+
+            if (programKey == 1)
+            {
+                Console.Clear();
+
+                Console.WriteLine("The organizer has the next functions:\n" + "1. Notes\n" +
+                    "2. Open calculator\n" + "3. Wheather\n" + "3. To do\n" + "Please make your choose.");
+
+                switch (Helper.ReadCommandsInt())
+                {
+                    case 1: NotesMenu(); break;
+                    case 2: OpenCalculator(); break;
+                    default:
+                        Console.WriteLine("Unknown command"); break;
+
+
+                }
+
+            }
+            else if (programKey == 0)
+            { Environment.Exit(0); }
+            else
+            {
+                Console.WriteLine("Unknown command");
+            }
+        }
+        public void OpenCalculator()
+        {
+        }
+        public void NotesMenu()
+        {
+            Console.Clear();
+            FilesStreams fw = new FilesStreams(@"C:\SomeDir\hta2.txt");
+            Console.WriteLine("The organizer has the next functions:\n" + "1. Write text to file\n" + "2. Read from file\n"
+                + "3. Go to main menu\n"
+                + "Please make your choose.");
+
+
+            switch (Helper.ReadCommandsInt())
+            {
+                case 1:
+                    {
+
+                        fw.AddTextToFile();
+                        Console.Clear();
+                        NotesMenu();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Clear();
+                        fw.GetTextFromFileAsync();
+                        Console.WriteLine("\nAre you want exit to previous menu? Type anyone or End for exit from program");
+
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.End)
+                        { Environment.Exit(0); }
+                        else { Console.Clear(); NotesMenu(); }
+                        break;
+                    }
+                case 3: StartMenu(1); break;
+                default:
+                    Console.WriteLine("Unknown command");
+                    break;
+            }
+
+
+        }
+
+
+    }
+
+}
+
+
